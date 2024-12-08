c6d603c8f4bda4ac8d59a9bab7859339d0ec34fd
Brandon Murdoch <brandon.murdoch@sainsburys.co.uk>
2024-12-08 19:18:19 +0000

    Day 07 part 2

--- src/main/java/challenge/_2024/D07.java
+++ src/main/java/challenge/_2024/D07.java
@@ -60,6 +60,41 @@ public class D07 extends Solution {
 
   public void partTwo() {
     log.info("# Part 2 #");
+    long sum = lines.stream().mapToLong(this::processLinePartTwo).sum();
+    log.info(sum);
+  }
+
+  private long processLinePartTwo(String s) {
+    String[] split = s.split(": ");
+    long target = Long.parseLong(split[0]);
+    List<Integer> operands = Arrays.stream(split[1].split(" ")).map(Integer::parseInt).toList();
+
+    int n = operands.size();
+    int totalCombinations = (int) Math.pow(3, n - 1);
+    for (int i = 0; i < totalCombinations; i++) {
+      long number = operands.get(0);
+      int temp = i;
+      for (int j = 0; j < n - 1; j++) {
+        int opIndex = temp % 3;
+        temp /= 3;
+        switch (opIndex) {
+          case 0:
+            number *= operands.get(j + 1);
+            break;
+          case 1:
+            number += operands.get(j + 1);
+            break;
+          case 2:
+            number = Long.parseLong(number + "" + operands.get(j + 1));
+            break;
+          default:
+        }
+      }
+      if (number == target) {
+        return target;
+      }
+    }
+    return 0L;
   }
 
   public void lore() {
