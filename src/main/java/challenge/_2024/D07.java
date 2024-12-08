b7e5feff7c5c38babcae5a1f8a538a5cefa545a9
Brandon Murdoch <brandon.murdoch@sainsburys.co.uk>
2024-12-08 19:15:49 +0000

    Day 07 part 1

--- src/main/java/challenge/_2024/D07.java
+++ src/main/java/challenge/_2024/D07.java
@@ -3,8 +3,6 @@ package challenge._2024;
 import base.Solution;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -26,16 +24,38 @@ public class D07 extends Solution {
 
   public void partOne() {
     log.info("# Part 1 #");
-    long sum = lines.stream().mapToLong(this::processLine).sum();
+    long sum = lines.stream().mapToLong(this::processLinePartOne).sum();
     log.info(sum);
   }
 
-  private long processLine(String s) {
+  private long processLinePartOne(String s) {
     String[] split = s.split(": ");
     long target = Long.parseLong(split[0]);
     List<Integer> operands = Arrays.stream(split[1].split(" ")).map(Integer::parseInt).toList();
 
-    return target;
+    int n = operands.size();
+    int totalCombinations = (int) Math.pow(2, n - 1);
+    for (int i = 0; i < totalCombinations; i++) {
+      long number = operands.get(0);
+      int temp = i;
+      for (int j = 0; j < n - 1; j++) {
+        int opIndex = temp % 2;
+        temp /= 2;
+        switch (opIndex) {
+          case 0:
+            number *= operands.get(j + 1);
+            break;
+          case 1:
+            number += operands.get(j + 1);
+            break;
+          default:
+        }
+      }
+      if (number == target) {
+        return target;
+      }
+    }
+    return 0L;
   }
 
   public void partTwo() {
