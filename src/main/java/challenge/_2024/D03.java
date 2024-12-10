a2642ddfa30872a914b9bc03e46875322cc57566
Brandon Murdoch <brandon.murdoch@sainsburys.co.uk>
2024-12-10 22:34:51 +0000

    Day 03 cleanup and lore

--- src/main/java/challenge/_2024/D03.java
+++ src/main/java/challenge/_2024/D03.java
@@ -26,31 +26,16 @@ public class D03 extends Solution {
 
   public void partOne() {
     log.info("# Part 1 #");
+    log.info(lore);
     Pattern r = Pattern.compile("mul\\((\\d{1,3},\\d{1,3})\\)");
-    List<String> result = new ArrayList<>();
-    for (String line : lines) {
-      Matcher m = r.matcher(line);
-      while (m.find()) {
-        result.add(m.group());
-      }
-    }
-    int sum = 0;
-    for (String s : result) {
-      sum += calculateMulValue(s);
-    }
+    int sum = parseIntoCommands(r).stream().mapToInt(this::calculateMulValue).sum();
     log.info(sum);
   }
 
   public void partTwo() {
     log.info("# Part 2 #");
     Pattern r = Pattern.compile("mul\\((\\d{1,3},\\d{1,3})\\)|do\\(\\)|don't\\(\\)");
-    List<String> result = new ArrayList<>();
-    for (String line : lines) {
-      Matcher m = r.matcher(line);
-      while (m.find()) {
-        result.add(m.group());
-      }
-    }
+    List<String> result = parseIntoCommands(r);
     int sum = 0;
     boolean active = true;
     for (String s : result) {
@@ -68,12 +53,22 @@ public class D03 extends Solution {
     log.info(sum);
   }
 
+  private List<String> parseIntoCommands(Pattern r) {
+    List<String> commands = new ArrayList<>();
+    for (String line : lines) {
+      Matcher m = r.matcher(line);
+      while (m.find()) {
+        commands.add(m.group());
+      }
+    }
+    return commands;
+  }
+
   private int calculateMulValue(String mulString) {
-    String[] split = mulString.split("mul\\(");
-    String[] split1 = split[1].split(",");
-    int num1 = Integer.parseInt(split1[0]);
-    String[] split2 = split1[1].split("\\)");
-    int num2 = Integer.parseInt(split2[0]);
+    mulString = mulString.replace("mul(", "").replace(")", "");
+    String[] split = mulString.split(",");
+    int num1 = Integer.parseInt(split[0]);
+    int num2 = Integer.parseInt(split[1]);
     return num1 * num2;
   }
 
