390c8d5ecb9f9444e12c6542dfcd14fa5a4024a8
Brandon Murdoch <brandon.murdoch@sainsburys.co.uk>
2024-12-20 14:06:27 +0000

    Day 09 part 2 with a lot of cleanup!

--- src/main/java/challenge/_2024/D09.java
+++ src/main/java/challenge/_2024/D09.java
@@ -25,40 +25,14 @@ public class D09 extends Solution {
   public void partOne() {
     log.info("# Part 1 #");
 
-    String diskMap = lines.get(0);
-
-    String[] arr = parseIntoFileArray(diskMap);
-    long checkSum = 0L;
-    for (int i = 0; i < arr.length; i++) {
-      if (!arr[i].equals(".")) {
-        checkSum += (long) i * Integer.parseInt(arr[i]);
-      }
-    }
-    log.info("Checksum: " + checkSum);
+    String disk = lines.get(0);
+    String[] arr = defragWithSingleMove(disk);
+    long checkSum = calculateChecksum(arr);
+    log.info("Checksum: {}", checkSum);
   }
 
-  public void partTwo() {
-    log.info("# Part 2 #");
-
-  }
-
-  private String[] parseIntoFileArray(String diskMap) {
-    List<String> files = new ArrayList<>();
-    int id = 0;
-    for (int i = 0; i < diskMap.length(); i++) {
-      String reading = diskMap.substring(i, i + 1);
-      int file = Integer.parseInt(reading);
-      if (i % 2 == 0) {
-        for (int j = 0; j < file; j++) {
-          files.add(String.valueOf(id));
-        }
-        id++;
-      } else {
-        for (int j = 0; j < file; j++) {
-          files.add(".");
-        }
-      }
-    }
+  private String[] defragWithSingleMove(String diskMap) {
+    List<String> files = parseIntoFilesList(diskMap);
 
     String[] arr = files.toArray(new String[0]);
     for (int i = arr.length - 1; i >= 0; i--) {
@@ -81,6 +55,105 @@ public class D09 extends Solution {
       }
     }
     return -1;
+  }
+
+  public void partTwo() {
+    log.info("# Part 2 #");
+
+    String disk = lines.get(0);
+    String[] arr = defragWithWholeFileMoves(disk);
+    long checkSum = calculateChecksum(arr);
+    log.info("Checksum: {}", checkSum);
+  }
+
+  private String[] defragWithWholeFileMoves(String disk) {
+    List<String> files = parseIntoFilesList(disk);
+
+    String[] arr = files.toArray(new String[0]);
+    String fileToMove = null;
+    List<String> lockedFiles = new ArrayList<>();
+    int fileToMoveSize = -1;
+    for (int i = arr.length - 1; i >= 0; i--) {
+      // log.info("Looking at index {} withvalue:{} file:{}", i, arr[i], String.join("", arr));
+      if (fileToMove == null) {
+        fileToMove = arr[i];
+        fileToMoveSize = 1;
+        continue;
+      }
+      if (arr[i].equals(fileToMove)) {
+        fileToMoveSize++;
+      } else {
+        // we're now at the end of that file.
+        if (lockedFiles.contains(fileToMove)) {
+          fileToMove = arr[i];
+          fileToMoveSize = 1;
+          continue;
+        }
+        int firstSpace = findFirstSpaceIndexWithGapSize(arr, fileToMoveSize);
+        // log.info("I want to move {} to {}", fileToMove, firstSpace);
+        if (firstSpace != -1 && firstSpace < i) {
+          lockedFiles.add(fileToMove);
+          for (int m = 0; m < fileToMoveSize; m++) {
+            arr[firstSpace + m] = fileToMove;
+            arr[1 + i + m] = ".";
+          }
+        }
+        if (arr[i].equals(".")) {
+          fileToMove = null;
+          fileToMoveSize = -1;
+        } else {
+          fileToMove = arr[i];
+          fileToMoveSize = 1;
+        }
+        // log.info("file:{}", String.join("", arr));
+      }
+    }
+    return arr;
+  }
+
+  private int findFirstSpaceIndexWithGapSize(String[] a, int size) {
+    int curGapSize = 0;
+    for (int i = 0; i < a.length; i++) {
+      if (a[i].equals(".")) {
+        curGapSize++;
+        if (curGapSize == size) {
+          return i - size + 1;
+        }
+      } else {
+        curGapSize = 0;
+      }
+    }
+    return -1;
+  }
+
+  private List<String> parseIntoFilesList(String diskMap) {
+    List<String> files = new ArrayList<>();
+    int id = 0;
+    for (int i = 0; i < diskMap.length(); i++) {
+      String reading = diskMap.substring(i, i + 1);
+      int file = Integer.parseInt(reading);
+      if (i % 2 == 0) {
+        for (int j = 0; j < file; j++) {
+          files.add(String.valueOf(id));
+        }
+        id++;
+      } else {
+        for (int j = 0; j < file; j++) {
+          files.add(".");
+        }
+      }
+    }
+    return files;
+  }
+
+  private long calculateChecksum(String[] arr) {
+    long checkSum = 0L;
+    for (int i = 0; i < arr.length; i++) {
+      if (!arr[i].equals(".")) {
+        checkSum += (long) i * Integer.parseInt(arr[i]);
+      }
+    }
+    return checkSum;
   }
 
   public void lore() {
