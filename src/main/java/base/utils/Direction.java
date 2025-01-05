c73629752d5a9c3a9a41b46f19a5bb0eeb0a65db
Brandon Murdoch <brandon.murdoch@sainsburys.co.uk>
2025-01-05 16:24:19 +0000

    Stuck on Day 16 part 2 ... can't work with multiple states going in the same direction

--- src/main/java/base/utils/Direction.java
+++ src/main/java/base/utils/Direction.java
@@ -46,6 +46,35 @@ public class Direction extends Coord {
     return null;
   }
 
+  public static Direction flip(Direction dir) {
+    if (dir.equals(Direction.N)) {
+      return Direction.S;
+    }
+    if (dir.equals(Direction.E)) {
+      return Direction.W;
+    }
+    if (dir.equals(Direction.S)) {
+      return Direction.N;
+    }
+    if (dir.equals(Direction.W)) {
+      return Direction.E;
+    }
+    return null;
+  }
+
+  public static String quickString(Direction d){
+    if (d.equals(N)){
+      return "^";
+    } else if (d.equals(E)){
+      return ">";
+    } else if (d.equals(S)){
+      return "v";
+    } else if (d.equals(W)) {
+      return "<";
+    }
+    return "X";
+  }
+
   public Direction(int dR, int dC) {
     super(dR, dC);
   }
--- src/main/java/challenge/_2024/D16.java
+++ src/main/java/challenge/_2024/D16.java
@@ -5,9 +5,11 @@ import static base.utils.FileUtil.inputToGrid;
 import base.Solution;
 import base.utils.Coord;
 import base.utils.Direction;
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.PriorityQueue;
@@ -55,9 +57,6 @@ public class D16 extends Solution {
     Set<State> visited = new HashSet<>();
     while (!priorityQueue.isEmpty()) {
       State curr = priorityQueue.poll();
-      log.info("-------------");
-      log.info("checking {} {}", curr.getCoord(), curr.getCost());
-      printPrioQ(priorityQueue);
 
       if (visited.contains(curr)) {
         continue;
@@ -70,15 +69,15 @@ public class D16 extends Solution {
 
       for (Direction dir : Direction.CARDINAL_DIRECTIONS) {
         Coord next = curr.getCoord().relative(dir);
-        if (!isCoordAWallOrOutOfBounds(grid, next) && !visited.contains(next)) {
+        if (!isCoordAWallOrOutOfBounds(grid, next) && !visited.contains(new State(next, dir))) {
           int newDistance = distances.get(curr) + 1;
           if (!curr.getDirection().equals(dir)) {
             newDistance += 1000;
           }
-          log.info("next could be {} with distance {}", next, newDistance);
           if (newDistance < distances.getOrDefault(new State(next, dir), Integer.MAX_VALUE)) {
             State nextState = new State(next, dir);
             nextState.setCost(newDistance);
+            nextState.setPrev(curr);
             distances.put(nextState, newDistance);
             priorityQueue.add(nextState);
           }
@@ -86,14 +85,6 @@ public class D16 extends Solution {
       }
     }
     return -1;
-  }
-
-  private void printPrioQ(PriorityQueue<State> priorityQueue) {
-    StringBuilder sb = new StringBuilder();
-    for (State item : priorityQueue) {
-      sb.append("[").append(item.getCoord()).append(" ").append(item.getCost()).append(" ").append("] | ");
-    }
-    log.info("Q has: {}", sb);
   }
 
   private boolean isCoordInInGrid(String[][] grid, Coord coord) {
@@ -111,7 +102,95 @@ public class D16 extends Solution {
 
   public void partTwo() {
     log.info("# Part 2 #");
+    String[][] grid = inputToGrid(lines);
+    Coord start = findChar(grid, "S");
+    Coord end = findChar(grid, "E");
 
+    List<State> cheap = findAllCheapPaths(grid, start, end);
+    log.info("----------------------");
+    log.info(cheap.size());
+    for (State state : cheap) {
+      state.printState(grid);
+    }
+    Set<Coord> coordsOnBestPath = new HashSet<>();
+    for (State state : cheap) {
+      coordsOnBestPath.addAll(state.getCoordsInPath());
+    }
+    log.info(coordsOnBestPath.size());
+  }
+
+  private List<State> findAllCheapPaths(String[][] grid, Coord start, Coord end) {
+    State startState = new State(start, Direction.E);
+
+    Map<State, Integer> distances = new HashMap<>();
+    distances.put(startState, 0);
+
+    PriorityQueue<State> priorityQueue = new PriorityQueue<>(
+        Comparator.comparingInt(State::getCost));
+    priorityQueue.add(new State(start, Direction.E));
+
+    Set<State> visited = new HashSet<>();
+    List<State> cheapestPaths = new ArrayList<>();
+    int bestPathSize = Integer.MAX_VALUE;
+    while (!priorityQueue.isEmpty()) {
+      printPrioQ(priorityQueue);
+      State curr = priorityQueue.poll();
+      log.info("checking {} cost {} dir {}", curr.getCoord(), curr.getCost(),
+          Direction.quickString(curr.getDirection()));
+      printCoordInGrid(grid, curr.getCoord(), curr.getDirection());
+      if (curr.getCost() > bestPathSize) {
+        continue;
+      }
+
+      if (curr.getCoord().equals(end)) {
+        log.info("found the end");
+        if (distances.get(curr) <= bestPathSize) {
+          bestPathSize = distances.get(curr);
+          cheapestPaths.add(curr);
+        }
+        continue;
+      }
+
+      if (visited.contains(curr)) {
+        continue;
+      }
+      visited.add(curr);
+
+      for (Direction dir : Direction.CARDINAL_DIRECTIONS) {
+        if (!dir.equals(Direction.flip(curr.getDirection()))) {
+          Coord next = curr.getCoord().relative(dir);
+          if (!isCoordAWallOrOutOfBounds(grid, next)) {
+            int newDistance = distances.get(curr) + 1;
+            if (!curr.getDirection().equals(dir)) {
+              newDistance += 1000;
+            }
+            if (newDistance <= checkDistanceForCoord(next, dir, distances)) {
+              log.info("next potential cold be {} {} {}", next, Direction.quickString(dir),
+                  newDistance);
+              State nextState = new State(next, dir);
+              nextState.setCost(newDistance);
+              nextState.setPrev(curr);
+              distances.put(nextState, newDistance);
+              priorityQueue.add(nextState);
+            }
+          }
+        }
+      }
+      log.info("checked all other directions...");
+    }
+    return cheapestPaths;
+  }
+
+  private int checkDistanceForCoord(Coord next, Direction dir, Map<State, Integer> distances) {
+    int lowestCost = Integer.MAX_VALUE;
+    for (State state : distances.keySet()) {
+      if (next.equals(state.getCoord()) && state.getDirection().equals(dir)) {
+        if (state.getCost() < lowestCost) {
+          lowestCost = state.getCost();
+        }
+      }
+    }
+    return lowestCost;
   }
 
   private Coord findChar(String[][] grid, String charToFind) {
@@ -127,6 +206,9 @@ public class D16 extends Solution {
 
   public static class State {
 
+    private final Logger log = LogManager.getLogger(State.class);
+
+    State prev;
     Coord coord;
     Direction direction;
     int cost;
@@ -160,6 +242,46 @@ public class D16 extends Solution {
       this.cost = cost;
     }
 
+    public State getPrev() {
+      return prev;
+    }
+
+    public void setPrev(State prev) {
+      this.prev = prev;
+    }
+
+    public void printState(String[][] grid) {
+      List<Coord> steps = new ArrayList<>();
+      steps.add(coord);
+      State tmpPrev = prev;
+      while (tmpPrev != null) {
+        steps.add(tmpPrev.getCoord());
+        tmpPrev = tmpPrev.getPrev();
+      }
+      for (int r = 0; r < grid.length; r++) {
+        StringBuilder sb = new StringBuilder();
+        for (int c = 0; c < grid[0].length; c++) {
+          if (steps.contains(new Coord(r, c))) {
+            sb.append("o");
+          } else {
+            sb.append(grid[r][c]);
+          }
+        }
+        log.info(sb);
+      }
+    }
+
+    public List<Coord> getCoordsInPath() {
+      List<Coord> steps = new ArrayList<>();
+      steps.add(coord);
+      State tmpPrev = prev;
+      while (tmpPrev != null) {
+        steps.add(tmpPrev.getCoord());
+        tmpPrev = tmpPrev.getPrev();
+      }
+      return steps;
+    }
+
     @Override
     public boolean equals(Object o) {
       if (this == o) {
@@ -175,6 +297,30 @@ public class D16 extends Solution {
     @Override
     public int hashCode() {
       return Objects.hash(coord, direction);
+    }
+  }
+
+  private void printPrioQ(PriorityQueue<State> priorityQueue) {
+    StringBuilder sb = new StringBuilder();
+    for (State item : priorityQueue) {
+      sb.append("[").append(item.getCoord()).append(" ").append(item.getCost()).append(" ")
+          .append(Direction.quickString(item.getDirection()))
+          .append("] | ");
+    }
+    log.info("Q has: {}", sb);
+  }
+
+  private void printCoordInGrid(String[][] grid, Coord coord, Direction dir) {
+    for (int r = 0; r < grid.length; r++) {
+      StringBuilder sb = new StringBuilder();
+      for (int c = 0; c < grid[0].length; c++) {
+        if (coord.equals(new Coord(r, c))) {
+          sb.append(Direction.quickString(dir));
+        } else {
+          sb.append(grid[r][c]);
+        }
+      }
+      log.info(sb);
     }
   }
 
