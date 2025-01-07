377dfeb33e5c9de46cfdd3a3d5f1d1e5d0ff3495
Brandon Murdoch <brandon.murdoch@sainsburys.co.uk>
2025-01-07 08:50:22 +0000

    Day 18 part 1

--- src/main/java/challenge/_2024/D18.java
+++ src/main/java/challenge/_2024/D18.java
@@ -2,9 +2,17 @@ package challenge._2024;
 
 import base.Solution;
 import base.utils.Coord;
+import base.utils.Direction;
 import base.utils.Pair;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.PriorityQueue;
+import java.util.Set;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -29,22 +37,64 @@ public class D18 extends Solution {
     this.lore();
     List<Coord> coordsInput = parseToCoords();
     Pair<Integer> rowAndColMax = findRowAndColMax(coordsInput);
-    String[][] grid = new String[rowAndColMax.getLeft()][rowAndColMax.getRight()];
+    String[][] grid = buildBaseGrid(rowAndColMax);
 
-    for (int r = 0; r < grid.length; r++) {
-      for (int c = 0; c < grid[0].length; c++) {
-        grid[r][c] = ".";
-      }
-    }
-    //for (int i = 0; i < 1024 - 1; i++) {
-    for (int i = 0; i < 12 - 1; i++) {
+    for (int i = 0; i < 1024; i++) {
       Coord c = coordsInput.get(i);
       grid[c.r()][c.c()] = "#";
     }
+    Coord start = new Coord(0, 0);
+    Coord end = new Coord(rowAndColMax.getLeft(), rowAndColMax.getRight());
     printGrid(grid);
+    log.info(findPathSize(grid, start, end));
+  }
 
-    log.info(rowAndColMax.getLeft() + " " + rowAndColMax.getRight());
+  private int findPathSize(String[][] grid, Coord start, Coord end) {
+    Map<Coord, Integer> distances = new HashMap<>();
+    distances.put(start, 0);
+    PriorityQueue<Coord> priorityQueue = new PriorityQueue<>(
+        Comparator.comparingInt(c -> distances.getOrDefault(c, Integer.MAX_VALUE)));
+    priorityQueue.add(start);
 
+    Set<Coord> visited = new HashSet<>();
+
+    while (!priorityQueue.isEmpty()) {
+      Coord curr = priorityQueue.poll();
+
+      if (visited.contains(curr)) {
+        continue;
+      }
+      visited.add(curr);
+
+      if (curr.equals(end)) {
+        return distances.get(curr);
+      }
+
+      for (Direction dir : Direction.CARDINAL_DIRECTIONS) {
+        Coord next = curr.relative(dir);
+        if (!isCoordAWallOrOutOfBounds(grid, next) && !visited.contains(next)) {
+          int newDistance = distances.get(curr) + 1;
+          if (newDistance < distances.getOrDefault(next, Integer.MAX_VALUE)) {
+            distances.put(next, newDistance);
+            priorityQueue.add(next);
+          }
+        }
+      }
+    }
+    return -1;
+  }
+
+  private boolean isCoordInInGrid(String[][] grid, Coord coord) {
+    return !(coord.r() < 0 || coord.r() >= grid.length ||
+        coord.c() < 0 || coord.c() >= grid[0].length);
+  }
+
+  private boolean isCoordAWallOrOutOfBounds(String[][] grid, Coord coord) {
+    if (isCoordInInGrid(grid, coord)) {
+      return "#".equals(grid[coord.r()][coord.c()]);
+    } else {
+      return true;
+    }
   }
 
   public void partTwo() {
@@ -52,7 +102,17 @@ public class D18 extends Solution {
 
   }
 
-  private List<Coord> parseToCoords(){
+  private String[][] buildBaseGrid(Pair<Integer> rowAndColMax) {
+    String[][] grid = new String[rowAndColMax.getLeft() + 1][rowAndColMax.getRight() + 1];
+    for (int r = 0; r < grid.length; r++) {
+      for (int c = 0; c < grid[0].length; c++) {
+        grid[r][c] = ".";
+      }
+    }
+    return grid;
+  }
+
+  private List<Coord> parseToCoords() {
     List<Coord> coords = new ArrayList<>();
     for (String line : lines) {
       String[] split = line.split(",");
@@ -61,20 +121,20 @@ public class D18 extends Solution {
     return coords;
   }
 
-  private Pair<Integer> findRowAndColMax(List<Coord> coords){
+  private Pair<Integer> findRowAndColMax(List<Coord> coords) {
     int rowMax = 0;
     int colMax = 0;
     for (Coord coord : coords) {
       rowMax = Math.max(rowMax, coord.r());
       colMax = Math.max(colMax, coord.c());
     }
-    return new Pair<>(rowMax +1, colMax +1);
+    return new Pair<>(rowMax, colMax);
   }
 
   private void printGrid(String[][] grid) {
-    for (int r = 0; r < grid.length; r++) {
+    for (String[] strings : grid) {
       StringBuilder sb = new StringBuilder();
-      for (String s : grid[r]) {
+      for (String s : strings) {
         sb.append(s);
       }
       log.info(sb.toString());
