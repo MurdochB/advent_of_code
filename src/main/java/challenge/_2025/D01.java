f2ae583a36e1a800cb270ac3aed0e610b45b7be0
Brandon Murdoch <brandon.murdoch@sainsburys.co.uk>
2025-12-01 11:30:39 +0000

    Cleaning up day 1 part 1,2

--- src/main/java/challenge/_2025/D01.java
+++ src/main/java/challenge/_2025/D01.java
@@ -2,6 +2,7 @@ package challenge._2025;
 
 import base.Solution;
 import base.utils.Pair;
+import base.utils.SplitPair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -11,6 +12,8 @@ public class D01 extends Solution {
 
   private static final String INPUT_FILE = "2025/inputs/01.txt";
   private static final String LORE_FILE = "2025/lore/01.txt";
+
+  private static final String OUTPUT = "Zeros count: {}";
 
   private D01(String inputFile, String loreFile) {
     super(inputFile, loreFile);
@@ -24,81 +27,92 @@ public class D01 extends Solution {
   public void partOne() {
     log.info("# Part 1 #");
     this.lore();
-    int dial = 50;
-    int zeroCount = 0;
+    Pair<Integer> dialAndZeros = new Pair<>(50, 0);
 
-    for (String line : lines) {
-      dial = processLine(dial, line);
-      log.info("DIAL: {}", dial);
-      if (dial == 0) {
-        zeroCount++;
-      }
-    }
-    log.info("Zero count: {}", zeroCount);
-  }
+    lines.forEach(l -> processLine(dialAndZeros, l));
 
-  private int processLine(int currentDial, String line) {
-    String operation = line.substring(0, 1);
-    int val = Integer.parseInt(line.substring(1));
-    val = val % 100;
-
-    if (operation.equals("R")) {
-      currentDial += val;
-    } else {
-      currentDial -= val;
-    }
-    if (currentDial < 0) {
-      currentDial = 100 + currentDial;
-    }
-    return currentDial % 100;
+    log.info(OUTPUT, dialAndZeros.getRight());
   }
 
   public void partTwo() {
     log.info("# Part 2 #");
-    Pair<Integer> dialAndZeroCount = new Pair<>(50, 0);
+    Pair<Integer> dialAndZeros = new Pair<>(50, 0);
 
-    for (String line : lines) {
-      dialAndZeroCount = processLinePt2(dialAndZeroCount, line);
-    }
-    log.info("Zero count: {}", dialAndZeroCount.getRight());
+    lines.forEach(l -> processLinePt2(dialAndZeros, l));
+
+    log.info(OUTPUT, dialAndZeros.getRight());
   }
 
-  private Pair<Integer> processLinePt2(Pair<Integer> dialAndZeroCount, String line) {
-    String operation = line.substring(0, 1);
-    int val = Integer.parseInt(line.substring(1));
-    log.info("dial {} | rotate {} {}", dialAndZeroCount.getLeft(), operation, val);
+  private void processLine(Pair<Integer> dialAndZeros, String line) {
+    // parse step into ["L/R", 1-9999]
+    SplitPair<String, Integer> step = parseStep(line);
+    int val = step.getRight() % 100;
 
-    int magnitude = val / 100;
+    int dial = dialAndZeros.getLeft();
 
-    val = val % 100;
-
-    boolean wasZeroAlready = dialAndZeroCount.getLeft() == 0;
-    int currentDial = dialAndZeroCount.getLeft();
-    int currentZeros = dialAndZeroCount.getRight();
-    currentZeros += magnitude;
-
-    if (operation.equals("R")) {
-      currentDial += val;
+    if (step.getLeft().equals("R")) {
+      dial += val;
     } else {
-      currentDial -= val;
+      dial -= val;
     }
-    if (currentDial < 0) {
-      if (!wasZeroAlready) {
-        currentZeros++;
-      }
-      currentDial = 100 + currentDial;
-    } else if (currentDial >= 100) {
-      currentZeros++;
+    dial = normaliseDial(dial);
+    dialAndZeros.setLeft(dial);
+    if (dial == 0) {
+      dialAndZeros.setRight(dialAndZeros.getRight() + 1);
     }
-    if (currentDial == 0) {
-      currentZeros++;
-    }
-    dialAndZeroCount.setLeft(currentDial % 100);
-    dialAndZeroCount.setRight(currentZeros);
-    log.info("dial {} | zeros {}", dialAndZeroCount.getLeft(), dialAndZeroCount.getRight());
-    return dialAndZeroCount;
   }
 
+  private void processLinePt2(Pair<Integer> dialAndZeros, String line) {
+    // parse step into ["L/R", 1-9999]
+    SplitPair<String, Integer> step = parseStep(line);
+    int val = step.getRight() % 100;
+
+    int magnitude = step.getRight() / 100;
+    dialAndZeros.setRight(dialAndZeros.getRight() + magnitude);
+
+    int dial = dialAndZeros.getLeft();
+    if (step.getLeft().equals("R")) {
+      dial += val;
+    } else {
+      dial -= val;
+    }
+    Pair<Integer> newDial = normaliseAndCountCrosses(dial);
+
+    dialAndZeros.setLeft(newDial.getLeft());
+    dialAndZeros.setRight(dialAndZeros.getRight() + newDial.getRight());
+
+    log.info(line + " |" + dialAndZeros.getLeft() + " " + dialAndZeros.getRight());
+  }
+
+  private SplitPair<String, Integer> parseStep(String line) {
+    String op = line.substring(0, 1);
+    int val = Integer.parseInt(line.substring(1));
+
+    return new SplitPair<>(op, val);
+  }
+
+  private int normaliseDial(int dial) {
+    if (dial < 0) {
+      return 100 + dial;
+    }
+    return dial % 100;
+  }
+
+  private Pair<Integer> normaliseAndCountCrosses(int dial) {
+    if (dial < 0) {
+      return new Pair<>(100 + dial, 1);
+    } else if (dial >= 100) {
+      dial %= 100;
+      return new Pair<>(dial, 1);
+    }
+    return new Pair<>(dial, 0);
+  }
+
+  /*
+  2025-12-01 09:34:51,722 INFO  Zero count: 1007
+  2025-12-01 09:34:51,722 INFO  # Part 2 #
+  2025-12-01 09:34:51,723 INFO  Zero count: 5820
+   */
   public void lore() {
     log.info(lore);
   }
